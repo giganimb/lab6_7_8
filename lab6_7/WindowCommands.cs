2131a8e00d124b046ff8dcc1bceb4010b635d638
giganimb <mcilyasat@mail.ru>
2021-03-20 16:53:39 +0300

    Добавьте файлы проекта.

---
+++ lab6_7/WindowCommands.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace lab6_7
+{
+    public class WindowCommands
+    {
+        static WindowCommands()
+        {
+            Add = new RoutedCommand("Exit", typeof(MainWindow));
+        }
+        public static RoutedCommand Add { get; set; }
+    }
+}
